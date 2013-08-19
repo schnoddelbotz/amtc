b1aaae935227008b26a0539ada52b6791a6c07bb
Jan Hacker <hacker@id.ethz.ch>
2013-08-20 00:56:57 +0200

    experimental WS-Man/DASH/AMT 9.0 support/hack

--- src/amtc.c
+++ src/amtc.c
@@ -37,14 +37,21 @@
 
 unsigned char *acmds[] = {
   /* SOAP/XML request bodies as included via amt.h */
-  cmd_info,cmd_powerup,cmd_powerdown,cmd_powerreset,cmd_powercycle 
+  cmd_info,cmd_powerup,cmd_powerdown,cmd_powerreset,cmd_powercycle,
+  /* WS-MAN / DASH / AMT9.0 versions */
+  wsman_info, wsman_up,wsman_down,wsman_reset,wsman_reset, wsman_info_step2
 }; 
 const char *hcmds[] = {
   "INFO","POWERUP","POWERDOWN","POWERRESET","POWERCYCLE"
 }; 
-const char *powerstate[] = {
+const char *powerstate[] = { /* AMT */
  "S0 (on)", "S1 (cpu stop)", "S2 (cpu off)", "S3 (sleep)",
  "S4 (hibernate)", "S5 (sotf-off)", "S4/S5", "MechOff"
+};
+const char *wspowerstate[] = { /* WS-MAN */
+ "1", "2", "S0 (on)", "4",
+ "5", "6", "7", "8",
+ "S5 (soft-off)"
 };
 char portnames[3390][8];
 struct host {
@@ -66,45 +73,56 @@ struct MemoryStruct {
 void build_hostlist(int,char**);
 void dump_hostlist();
 void process_hostlist();
+void get_amt_pw();
 int  get_amt_response_status(void*);
 int  probe_one_hostport(int,int,int);
+int  get_enum_context(void*,void*);
+
 static size_t write_memory_callback(void*,size_t,size_t,void*);
 
+FILE  *pwfile;
 sem_t mutex;
 int   verbosity = 0;
 int   scan_ssh = 0;
 int   scan_rdp = 0;
 int   cmd = 0;
 int   numHosts = 0;
+int   quiet = 0;
 int   threadsRunning = 0;
 int   connectTimeout = 5;
 int   waitDelay = -1;
 int   maxThreads = 40;
 int   produceJSON = 0;
+int   useWsmanShift = 0;
+char  amtpasswdfile[255];
 char  amtpasswd[32];
-char  *amtpasswdp;
 char  gre[64];
+char  *amtpasswdfilep = NULL;
+char  *amtpasswdp = (char*)&amtpasswd;
 char  *grep = (char*)&gre;
+// FIXME xclude
 
 ///////////////////////////////////////////////////////////////////////////////
 int main(int argc,char **argv,char **envp) {
   int c;
-  amtpasswdp = (char*)&amtpasswd;
     
-  while ((c = getopt(argc, argv, "viudrRSWjs:t:w:m:")) != -1)
+  while ((c = getopt(argc, argv, "IUDRCdqvjsrp:t:w:m:")) != -1)
   switch (c) {
-    case 'v': verbosity += 1;         break;
-    case 'j': produceJSON = 1;        break;
-    case 'i': cmd = CMD_INFO;         break; 
-    case 'u': cmd = CMD_POWERUP;      break; 
-    case 'd': cmd = CMD_POWERDOWN;    break; 
-    case 'r': cmd = CMD_POWERCYCLE;   break; 
-    case 'R': cmd = CMD_POWERRESET;   break; 
-    case 'S': scan_ssh = 1;           break; 
-    case 'W': scan_rdp = 1;           break; 
+    case 'I': cmd = CMD_INFO;                break; 
+    case 'U': cmd = CMD_POWERUP;             break; 
+    case 'D': cmd = CMD_POWERDOWN;           break; 
+    case 'C': cmd = CMD_POWERCYCLE;          break; 
+    case 'R': cmd = CMD_POWERRESET;          break; 
+    case 's': scan_ssh = 1;                  break; 
+    case 'r': scan_rdp = 1;                  break; 
+    case 'j': produceJSON = 1;               break;
+    case 'q': quiet = 1;                     break;
+    case 'm': maxThreads = atoi(optarg);     break; 
+    case 'p': amtpasswdfilep = optarg;       break; 
     case 't': connectTimeout = atoi(optarg); break; 
-    case 'm': maxThreads = atoi(optarg); break; 
-    case 'w': waitDelay = atoi(optarg); break; 
+    case 'v': verbosity += 1;                break;
+    case 'd': useWsmanShift = 5;             break;
+    case 'w': waitDelay = atoi(optarg);      break; 
   }
 
   if (waitDelay==-1 && cmd == CMD_POWERUP) {
@@ -122,10 +140,6 @@ int main(int argc,char **argv,char **envp) {
     printf("%s", amtc_usage);
     exit(2);
   }
-  if (!(amtpasswdp=getenv("AMT_PASSWORD"))) {
-    printf("Set your AMT_PASSWORD environment variable [or use -p].\n");
-    exit(3);
-  }
 
   strcpy(portnames[SCANRESULT_NONE_OPEN],"none"); /* no open ports found */
   strcpy(portnames[SCANRESULT_NONE_RUN], "skipped"); /* skipped, eg. pwrd off */
@@ -139,6 +153,7 @@ int main(int argc,char **argv,char **envp) {
     snprintf(grep,sizeof gre,"<b:RemoteControlResponse><b:Status>");
   }
 
+  get_amt_pw();
   build_hostlist(argc,argv);
   sem_init(&mutex, 0, 1);
   curl_global_init(CURL_GLOBAL_ALL);
@@ -168,7 +183,9 @@ static void *process_single_client(void* num) {
   // http://curl.haxx.se/libcurl/c/threaded-ssl.html
   // --> produces lots of openssl deprecation warnings...
 
-  if (cmd==CMD_INFO)
+  if (useWsmanShift!=0) {
+
+  } else if (cmd==CMD_INFO)
     headers = curl_slist_append(headers, "SOAPAction: \"http://schemas.intel" \
            ".com/platform/client/RemoteControl/2004/01#GetSystemPowerState\"");
   else
@@ -187,7 +204,7 @@ static void *process_single_client(void* num) {
   curl_easy_setopt(curl, CURLOPT_USERNAME, "admin"  );
   curl_easy_setopt(curl, CURLOPT_PASSWORD, amtpasswdp);
   curl_easy_setopt(curl, CURLOPT_POST , 1);
-  curl_easy_setopt(curl, CURLOPT_POSTFIELDS , acmds[cmd]);
+  curl_easy_setopt(curl, CURLOPT_POSTFIELDS , acmds[cmd+useWsmanShift]);
   curl_easy_setopt(curl, CURLOPT_HTTPHEADER , headers);
   // http://stackoverflow.com/questions/9191668/error-longjmp-causes-uninitialized-stack-frame
   curl_easy_setopt(curl, CURLOPT_NOSIGNAL, 1); 
@@ -195,15 +212,39 @@ static void *process_single_client(void* num) {
   res = curl_easy_perform(curl);
   curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &http_code);
 
+  // (just another) hack for ws-man
+  // a enumeration context needs to be requested and pulled
+  char enumCtx[8192];
+  char enumTxt[8192];
+  if (http_code==200 && useWsmanShift!=0) {
+    if (get_enum_context(chunk.memory,&enumCtx)) {
+      sprintf(enumTxt, (char*)wsman_info_step2, enumCtx);
+      curl_easy_setopt(curl, CURLOPT_POSTFIELDS , enumTxt);
+      res = curl_easy_perform(curl);
+      curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &http_code);
+    } else {
+      printf("YIKES. fixme wsman\n");
+    }
+  } 
+
   char umsg[100];
   const char* umsgp = (char*)&umsg;
 
-  if(res != CURLE_OK || http_code!=200) {
+  /*if (useWsmanShift) {
+    printf("ws success.\n");
+  }
+  else */if(res != CURLE_OK || http_code!=200) {
     amt_result = -999;
     umsgp = curl_easy_strerror(res);
   }
   else {
     amt_result = get_amt_response_status(chunk.memory);
+    if(useWsmanShift)
+    snprintf((char*)&umsg, sizeof umsg, "OK %s%s", 
+      (cmd==CMD_INFO) ? wspowerstate[amt_result] : "",
+      (cmd!=CMD_INFO && amt_result==0) ? "success" : "" 
+    );
+    else
     snprintf((char*)&umsg, sizeof umsg, "OK %s%s", 
       (cmd==CMD_INFO) ? powerstate[amt_result & 0x0f] : "",
       (cmd!=CMD_INFO && amt_result==0) ? "success" : "" 
@@ -252,14 +293,34 @@ static void *process_single_client(void* num) {
 int get_amt_response_status(void* chunk) {
   int response = -9;
   char *pos = NULL;
-  pos = strstr(chunk, grep);
-  if (pos==NULL) {
-    response = -99; // no match -- may be wrong amt version, too
+  if (useWsmanShift) {
+    //<h:PowerState>8 ... only info 4 now
+    pos = strstr(chunk, "<h:PowerState>");
+    if (pos==NULL) {
+    } else {
+      pos = pos + strlen("<h:PowerState>");
+      response = atoi(pos);
+    }
   } else {
-    pos = pos + strlen(grep);
-    response = atoi(pos);
+    pos = strstr(chunk, grep);
+    if (pos==NULL) {
+      response = -99; // no match -- may be wrong amt version, too
+    } else {
+      pos = pos + strlen(grep);
+      response = atoi(pos);
+    }
   }
   return response;
+}
+
+int get_enum_context(void* chunk,void* result) {
+  char *pos = NULL;
+  pos = strstr(chunk, "<g:EnumerateResponse><g:EnumerationContext>"/*len=44*/);
+  if (pos==NULL) 
+    return 0;
+  else 
+    strncpy(result, pos+43, 36 /*ctx str len,fixme: hopefully*/ );
+  return 1;
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -305,9 +366,11 @@ int probe_one_hostport(int hostid, int port, int alreadyFound) {
   servaddr.sin_port=htons(port);
 
   c=connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr));
+  close(sockfd);
   if (verbosity>1)
     printf("SCAN %d on %15s - %d\n", port, hostlist[hostid].hostname, c);
-
+  
+  // read ssh greeting?
   return (c==0) ? port : SCANRESULT_NONE_OPEN;
 }
 
@@ -358,7 +421,10 @@ void build_hostlist(int argc,char **argv) {
   int i,h=0;
   for (i=optind; i<argc; i++) {
     hostlist[h].http_result = -2;
-    sprintf(hostlist[h].url,"http://%s:16992/RemoteControlService",argv[i]);
+    if (useWsmanShift)
+      sprintf(hostlist[h].url,"http://%s:16992/wsman",argv[i]);
+    else 
+      sprintf(hostlist[h].url,"http://%s:16992/RemoteControlService",argv[i]);
     sprintf(hostlist[h].hostname,"%s",argv[i]);
     // FIXME should create ip here for scan
     h++;
@@ -378,6 +444,7 @@ void dump_hostlist() {
     printf("}\n");
   } else {
     for(a = 0; a < numHosts; a++) {
+      if (!quiet || (quiet && hostlist[a].http_result!=200))
       printf("%s %-15s OS:%-7s AMT:%04d HTTP:%03d %s\n",
         hcmds[cmd], hostlist[a].hostname, portnames[hostlist[a].osport],
          hostlist[a].amt_result, hostlist[a].http_result, hostlist[a].usrmsg);
@@ -385,3 +452,24 @@ void dump_hostlist() {
   }
 }
 
+///////////////////////////////////////////////////////////////////////////////
+void get_amt_pw() {
+  int l;
+  if (amtpasswdfilep!=NULL) {
+    pwfile = fopen(amtpasswdfilep, "r");
+    if(pwfile == NULL) {
+      perror("Error opening password file");
+      exit(4);
+    }
+    if( fgets (amtpasswd, sizeof amtpasswd, pwfile)!=NULL ) {
+      l=strlen(amtpasswd);
+      amtpasswd[l-1]=0x00;
+    }
+    fclose(pwfile);
+    return;
+  }
+  if (!(amtpasswdp=getenv("AMT_PASSWORD"))) {
+    printf("Set your AMT_PASSWORD environment variable [or use -p].\n");
+    exit(3);
+  }
+}
