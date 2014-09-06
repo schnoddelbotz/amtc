ba06f9a37c5ed581fd20d8d78e3a4c975d3cb1fb
Jan Hacker <jan@hacker.ch>
2014-09-06 23:11:26 +0200

    data (=config) directory should exist after build

--- amtc-web2/build.sh
+++ amtc-web2/build.sh
@@ -17,7 +17,7 @@ function FetchIfNotExists {
   fi
 }
 
-mkdir -p js css lib
+mkdir -p js css lib data
 FetchIfNotExists js/jquery.min.js http://code.jquery.com/jquery-${JQUERY}.min.js
 FetchIfNotExists js/jquery.min.map http://code.jquery.com/jquery-${JQUERY}.min.map
 FetchIfNotExists js/handlebars.js http://builds.handlebarsjs.com.s3.amazonaws.com/handlebars-v${HANDLEBARS}.js
