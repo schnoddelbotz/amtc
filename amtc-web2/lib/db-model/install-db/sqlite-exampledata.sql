db0d3cef12d85a3f900067eaaccea56c55c6bd30
Jan Hacker <jan@hacker.ch>
2014-09-07 23:34:23 +0200

    more meaningful exampledata

--- amtc-web2/lib/db-model/install-db/sqlite-exampledata.sql
+++ amtc-web2/lib/db-model/install-db/sqlite-exampledata.sql
@@ -9,10 +9,12 @@ INSERT INTO "notifications" VALUES(NULL,1409988624,'envelope','daily status repo
 INSERT INTO "notifications" VALUES(NULL,1410004353,'warning','More than 10 hosts unreachable!');
 
 -- some amtc option sets
-INSERT INTO "optionsets" VALUES(1,'AMT v6 - No TLS','Uses EOI',0,0,1,1,0,0,40,5,'/tmp/test1.pass','');
-INSERT INTO "optionsets" VALUES(2,'AMT v9 - No TLS','Uses DASH',0,1,1,1,0,0,40,5,'/tmp/test2.pass','');
-INSERT INTO "optionsets" VALUES(3,'AMT v9 - with TLS, no cert check','Uses DASH',0,1,1,1,1,1,40,5,'/tmp/test2.pass','');
-INSERT INTO "optionsets" VALUES(4,'AMT v9 - with TLS, verify cert','Uses DASH',0,1,1,1,1,0,40,5,'/tmp/test2.pass','/tmp/my.ca.crt');
+INSERT INTO "optionsets" VALUES(1,'DASH / No TLS','Uses DASH',0,1,1,1,0,0,200,10,'/tmp/test2.pass','');
+INSERT INTO "optionsets" VALUES(2,'DASH / TLS / VerifyCertSkip','Skips TLS certificate verification',0,1,1,1,1,1,175,10,'/tmp/test2.pass','');
+INSERT INTO "optionsets" VALUES(3,'DASH / TLS / VerifyCert','Most secure optionset',0,1,1,1,1,0,150,15,'/tmp/test3.pass','/tmp/my.ca.crt');
+INSERT INTO "optionsets" VALUES(4,'EOI / No TLS - AMT v5','For old hardware with AMT 5.0 (around 2008)',1,0,0,0,0,0,100,10,'/tmp/testv5.pass',NULL);
+INSERT INTO "optionsets" VALUES(5,'EOI / No TLS - AMT v6-8','EOI + No TLS = the fastest. But only does digest auth via http.',0,0,1,1,0,0,250,6,'/tmp/test1.pass','');
+
 -- example OUs ...
 -- comes in via minimal already: 
 -- INSERT INTO "ous" VALUES(1,NULL,NULL,'ROOT','root');
@@ -20,8 +22,8 @@ INSERT INTO "optionsets" VALUES(4,'AMT v9 - with TLS, verify cert','Uses DASH',0
 INSERT INTO "ous" VALUES(3,2,NULL,'E Floor','All rooms on E floor');
 INSERT INTO "ous" VALUES(4,2,NULL,'D Floor','All rooms on D floor');
 INSERT INTO "ous" VALUES(5,1,NULL,'Course rooms','Playground');
-INSERT INTO "ous" VALUES(6,5,NULL,'Room A1','North');
-INSERT INTO "ous" VALUES(7,5,NULL,'Room A2','South');
+INSERT INTO "ous" VALUES(6,5,NULL,'Room A1','No optionset yet');
+INSERT INTO "ous" VALUES(7,5,NULL,'Room A2','Testing ... No optionset, too');
 -- and some real rooms
 INSERT INTO "ous" VALUES(8,3,3,'E 19','');
 INSERT INTO "ous" VALUES(9,3,2,'E 26.1','');
