e34398f47ca8a98e706a23cf6ae830893d8cfb63
schnoddelbotz <jan@hacker.ch>
2015-04-05 04:24:02 +0200

    squeeze built web-app (concat/uglify); can be overridden:
    MERGE_JS=NO MERGE_CSS=NO HAVE_NODEJS=NO make

--- amtc-web/js/compile-template.js
+++ amtc-web/js/compile-template.js
@@ -3,10 +3,11 @@
 // http://emberjs.com/blog/2015/02/05/compiling-templates-in-1-10-0.html
 
 var fs = require('fs');
+var UglifyJS = require("uglify-js");
 var compiler = require('./ember-template-compiler');
 var input = fs.readFileSync(process.argv[2], { encoding: 'utf8' });
 var template = compiler.precompile(input, false);
 var output = "Ember.TEMPLATES['"+process.argv[3]+"'] = Ember.HTMLBars.template(" + template + ');';
-
-fs.appendFileSync('./js/compiled-templates.js', output, { encoding: 'utf8' });
+var minified = UglifyJS.minify(output, {fromString: true});
+fs.appendFileSync('./js/compiled-templates.js', minified.code, { encoding: 'utf8' });
 
